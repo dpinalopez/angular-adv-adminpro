aa36a2fdd3490b0c743f2fe73ade679bd94c97c7
dpinalopez68 <dpinalopez68@gmail.com>
2020-07-20 20:08:43 +0200

    fin seccion 4

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from "@angular/router";
+
+
+
+@NgModule({
+  declarations: [
+    ProgressComponent,
+    Grafica1Component,
+    DashboardComponent,
+    PagesComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    ProgressComponent,
+    Grafica1Component,
+    DashboardComponent,
+    PagesComponent,
+  ],
+})
+export class PagesModule { }
